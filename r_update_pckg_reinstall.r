2fc4b1b6ebefaec1b0f9a7a0e0037993cb68a550
Jane Yang <jane.juen.yang@gmail.com>
2019-01-28 11:36:48 -0600

    add BigQuery, remove OAF shinyapp, reorg

--- r_update_pckg_reinstall.r
+++ r_update_pckg_reinstall.r
@@ -1,11 +1,14 @@
 # R upgrade package reinstallation
 # because R is silly and removes all your packages when you upgrade
 # written by: jane yang 
-# last edited: jan 25, 2019
+# last edited: jan 28, 2019
 
 # install core packages
 install.packages(c("tidyverse", "devtools", "lubridate", "knitr", "rmarkdown", 
-    "RODBC", "RJDBC", "RMySQL", "ggthemes", "googlesheets", "keyring"))
+    "ggthemes", "googlesheets", "keyring"))
+
+# install database connector packages
+install.packages(c("RODBC", "RJDBC", "RMySQL", "bigrquery"))
 
 # install packages related to shiny
 install.packages(c("shiny", "shinythemes", "shinyBS", "rsconnect"))
@@ -24,11 +27,6 @@ install.packages(c("circlize", "wordcloud"))
 
 # install packages for formatting
 install.packages(c("xkcd", "extrafont", "acepack"))
-
-# configure computer to allow app uploads to oaf's shinyapps.io account
-# rsconnect::setAccountInfo(name='oneacrefund',
-#     token='BFAC4158F910A46EDAA76F865333E4D1',
-#     secret='Pb4CP680U1rcoGCgJhKlh5PZ3VGHR3+Bn/A0MNVM')
 
 # Alternatively, you can follow the instructions at this link to store, upgrade
 # and then reload all the packages you have installed:
