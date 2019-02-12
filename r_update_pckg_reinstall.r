9e60650bce093e2ca518a4d731599ee99ebfecda
Jane Yang <jane.juen.yang@gmail.com>
2019-02-12 13:28:09 -0800

    add directlabel for ggplot2

--- r_update_pckg_reinstall.r
+++ r_update_pckg_reinstall.r
@@ -1,11 +1,11 @@
 # R upgrade package reinstallation
 # because R is silly and removes all your packages when you upgrade
 # written by: jane yang 
-# last edited: feb 6, 2019
+# last edited: feb 12, 2019
 
 # install core packages
 install.packages(c("tidyverse", "devtools", "lubridate", "knitr", "rmarkdown", 
-    "ggthemes", "googlesheets", "googledrive", "keyring"))
+    "ggthemes", "googlesheets", "googledrive", "keyring", "directlabels"))
 
 # install database connector packages
 install.packages(c("RODBC", "RJDBC", "RMariaDB", "bigrquery", "rgeolocate"))
