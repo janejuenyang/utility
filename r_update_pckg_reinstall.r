c385eda5cb5c91c4f427669f644f6fca8217c5bf
Jane Yang <jane.juen.yang@gmail.com>
2019-04-10 18:56:22 -0500

    add packages for fx exploration

--- r_update_pckg_reinstall.r
+++ r_update_pckg_reinstall.r
@@ -1,7 +1,7 @@
 # R upgrade package reinstallation
 # because R is silly and removes all your packages when you upgrade
 # written by: jane yang 
-# last edited: apr 4, 2019
+# last edited: apr 10, 2019
 
 # install core packages
 install.packages(c("tidyverse", "devtools", "lubridate", "knitr", "rmarkdown", 
@@ -29,7 +29,7 @@ install.packages(c(
     # for multi-variate (e.g. anova) tests
     , "multcomp" 
     # for time series analysis
-    , "forecast", "seasonal", "forecastHybrid" 
+    , "forecast", "seasonal", "forecastHybrid", "tsbox"
     # for matching controls (e.g. propensity scores)
     , "optmatch", "MatchIt" 
     # for A/B testing
@@ -47,6 +47,9 @@ install.packages(c("circlize", "wordcloud", "networkD3"))
 
 # install packages for formatting
 install.packages(c("xkcd", "extrafont", "acepack"))
+
+# install miscellaneous dataset packages
+install.packages(c("quantmod"))
 
 # Alternatively, you can follow the instructions at this link to store, upgrade
 # and then reload all the packages you have installed:
